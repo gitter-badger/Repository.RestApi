583ad33ffe8bd5620d11e29860ea795aebf09cf6
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-15 07:17:47 -0600

    Update description and tags

--- Source/Repository.RestApi/Properties/AssemblyInfo.cs
+++ Source/Repository.RestApi/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Repository.NET - REST API Repository")]
-[assembly: AssemblyDescription("A Respository.NET implementation supporting REST.")]
+[assembly: AssemblyTitle("Repository.NET - REST API")]
+[assembly: AssemblyDescription("A Respository.NET implementation supporting REST APIs.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Scott Keith Thomas, Jr.")]
-[assembly: AssemblyProduct("Repository.NET - REST API Repository")]
+[assembly: AssemblyProduct("Repository.NET - REST API")]
 [assembly: AssemblyCopyright("Copyright © 2016 Scott Keith Thomas, Jr.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
